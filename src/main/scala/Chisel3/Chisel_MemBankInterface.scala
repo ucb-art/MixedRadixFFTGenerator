6429b8224177df327d42c5008bb88019f9f0a92a
Angie <shunshou@gmail.com>
2017-03-16 14:40:13 -0700

    finished membankinterface. used mux1h

--- src/main/scala/Chisel3/Chisel_MemBankInterface.scala
+++ src/main/scala/Chisel3/Chisel_MemBankInterface.scala
@@ -1,3 +1,68 @@
 package dspblocks.fft
 import chisel3._
+import dsptools.numbers._
+import dsptools.numbers.implicits._
 
+// Crossbar + Membanks
+
+class MemInputLane[T <: Data:Ring](dataType: => T, maxNumBanks: Int, maxDepth: Int) extends Bundle {
+  val din = Input(dataType)
+  val we = Input(Bool())
+  // Closed range: bank followed by addr
+  // TODO: Make semantics consistent -- all use [)
+  val loc = Input(new BankAddressBundle(Seq(maxNumBanks - 1, maxDepth - 1)))
+  override def cloneType = (new MemInputLane(dataType, maxNumBanks, maxDepth)).asInstanceOf[this.type]
+}
+
+class MemOutputLane[T <: Data:Ring](dataType: => T, maxNumBanks: Int, maxDepth: Int) extends Bundle {
+  val dout = Output(dataType)
+  // Closed range: bank followed by addr
+  val loc = Input(new BankAddressBundle(Seq(maxNumBanks - 1, maxDepth - 1)))
+  override def cloneType = (new MemOutputLane(dataType, maxNumBanks, maxDepth)).asInstanceOf[this.type] 
+}
+
+class MemBankInterfaceIO[T <: Data:Ring](dataType: T, maxNumBanks: Int, maxDepth: Int) extends Bundle {
+  val clk = Input(Clock())
+  val i = Input(Vec(maxNumBanks, new MemInputLane(dataType, maxNumBanks, maxDepth)))
+  val o = Output(Vec(maxNumBanks, new MemOutputLane(dataType, maxNumBanks, maxDepth)))
+  override def cloneType = (new MemBankInterfaceIO(dataType, maxNumBanks, maxDepth)).asInstanceOf[this.type]
+}
+
+class MemBankInterface[T <: Data:Ring](dataType: T, bankLengths: Seq[Int]) extends Module with DelayTracking {
+  // Read data valid 1 clk cycle after read address
+  // Have as many input ports as # of banks -- that then gets sorted
+  val moduleDelay = 1
+  val io = IO(new MemBankInterfaceIO(dataType, maxNumBanks = bankLengths.length, maxDepth = bankLengths.max))
+  val memBanks = Module(new MemBanks(dataType, bankLengths))
+
+  val writeBankSel = memBanks.io.bank.zipWithIndex map { case (_, bankIdx) =>
+    io.i.map { case lane => lane.loc.bank === bankIdx.U }
+  }
+
+  val readBankSel = memBanks.io.bank.zipWithIndex map { case (_, bankIdx) =>
+    io.o.map { case lane => lane.loc.bank === bankIdx.U }
+  }
+
+  memBanks.io.bank.zipWithIndex foreach { case (bankIo, bankIdx) =>
+    bankIo.din := Mux1H(io.i.zipWithIndex.map { case (lane, laneIdx) => 
+      (writeBankSel(bankIdx)(laneIdx), lane.din)
+    })
+    bankIo.we := Mux1H(io.i.zipWithIndex.map { case (lane, laneIdx) => 
+      (writeBankSel(bankIdx)(laneIdx), lane.we)
+    })
+    bankIo.waddr := Mux1H(io.i.zipWithIndex.map { case (lane, laneIdx) => 
+      (writeBankSel(bankIdx)(laneIdx), lane.loc.addr)
+    })
+    bankIo.raddr := Mux1H(io.o.zipWithIndex.map { case (lane, laneIdx) => 
+      (readBankSel(bankIdx)(laneIdx), lane.loc.addr)
+    })
+    bankIo.clk := io.clk
+  }
+
+  io.o.zipWithIndex foreach { case (lane, laneIdx) =>
+    lane.dout := Mux1H(memBanks.io.bank.zipWithIndex.map { case (bankIo, bankIdx) => 
+      (readBankSel(bankIdx)(laneIdx), bankIo.dout)
+    })
+  }
+
+}
