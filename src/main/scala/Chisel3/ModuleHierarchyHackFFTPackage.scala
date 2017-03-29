5dedae166834329c10dcf755aa0ce9dbd1d2b468
Angie <shunshou@gmail.com>
2017-03-28 20:30:14 -0700

    ensure that original chisel clock isn't being connected anywhere -- connect all submodule clock/reset to false

--- src/main/scala/Chisel3/ModuleHierarchyHackFFTPackage.scala
+++ src/main/scala/Chisel3/ModuleHierarchyHackFFTPackage.scala
@@ -1,3 +1,4 @@
 package dspblocks.fft
-abstract class Module extends chisel3.ModuleWithParentInfo
+import chisel3._
+abstract class Module extends chisel3.Module(override_clock = Some(false.B.asClock), override_reset = Some(false.B)) with chisel3.ModuleWithParentInfo
 abstract class BlackBox extends chisel3.BlackBox with chisel3.ModuleWithParentInfo with chisel3.util.HasBlackBoxInline
